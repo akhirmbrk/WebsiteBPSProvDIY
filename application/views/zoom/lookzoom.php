5b4c3967ea1ee3392d51de93f95ebc9b2afd4773
akhirmbrk <222011698@stis.ac.id>
2023-08-28 10:36:55 +0700

    Zoom Complete

--- application/views/zoom/lookzoom.php
+++ application/views/zoom/lookzoom.php
@@ -1,195 +1,200 @@
+<!-- Main container -->
+<main>
 
-    <!-- Main container -->
-    <main>
-
-      <header class="header header- bg-img" style="background-image: url(../assets/img/gallery/1.jpg)" >
+    <header class="header header- bg-img" style="background-image: url(<?= base_url() ?>assets/img/gallery/1.jpg)">
         <div class="header-info">
-          <div class="left">
-            <h1 class="header-title">Deskripsi Rapat Daring</h1>
-          </div>
+            <div class="left">
+                <h1 class="header-title">Deskripsi Rapat Daring</h1>
+            </div>
 
-          <div class="right flex-middle">
-           
-          </div>
+            <div class="right flex-middle">
+
+            </div>
         </div>
 
         <div class="header-action">
-          <div class="flexbox align-items-center gap-items-4">
-          
-          </div>
+            <div class="flexbox align-items-center gap-items-4">
 
-          <nav class="nav">
+            </div>
 
-          </nav>
+            <nav class="nav">
+
+            </nav>
         </div>
-      </header>
+    </header>
 
-      <div class="main-content">
+    <div class="main-content">
         <div class="card card-body">
-         
-		 
-		 
-		 
-		 
-		 <div class="row">
-	
 
-	<div class="col-12">
-		<?php echo $this->session->flashdata('info_form');  ?>
-              <div class="card-body">
-                <div class="row">
-					
-                 
-				  
-				  <?php if($lookzoom['idm']!=0) { ?>
-				  
-				  
-				  <div class="col-md-6">
-					
 
-                    <div class="row">
-                      <div class="form-group col-md-12">
-                        <label>Perihal Rapat Dalam Jaringan (Daring) </label>
-                        <h2><?php  echo $lookzoom['perihal']; ?></h2>
-                      </div>
+
+
+
+            <div class="row">
+
+
+                <div class="col-12">
+                    <?php echo $this->session->flashdata('info_form');  ?>
+                    <div class="card-body">
+                        <div class="row">
+
+
+
+                            <?php if($lookzoom['idm']!=0) { ?>
+
+
+                            <div class="col-md-6">
+
+
+                                <div class="row">
+                                    <div class="form-group col-md-12">
+                                        <label>Perihal Rapat Dalam Jaringan (Daring) </label>
+                                        <h2><?php  echo $lookzoom['perihal']; ?></h2>
+                                    </div>
+                                </div>
+                                <div class="row">
+
+                                    <div class="form-group col-md-12">
+                                        <label>Waktu Mulai</label>
+                                        <div class="input-group">
+
+                                        </div>
+                                        <div class="input-group">
+
+                                            <input type="text" name="tgl_start" class="form-control" readonly
+                                                value="<?php  echo $lookzoom['tgl_start_tgl']; ?>">
+                                            <span class="input-group-addon">
+                                                <i class="fa fa-calendar"></i>
+                                            </span>
+
+
+
+                                            <input type="text" name="time_start" class="form-control"
+                                                value="<?php  echo $lookzoom['tgl_start_time']; ?>" readonly>
+                                            <span class="input-group-addon"><i class="ti-timer"></i></span>
+                                        </div>
+                                    </div>
+
+
+
+
+                                </div>
+
+
+                                <div class="row">
+
+                                    <div class="form-group col-md-12">
+                                        <label>Waktu Selesai</label>
+                                        <div class="input-group">
+
+                                        </div>
+                                        <div class="input-group">
+
+                                            <input type="text" name="tgl_end" class="form-control" data-provide=""
+                                                value="<?php  echo $lookzoom['tgl_end_tgl']; ?>" readonly>
+                                            <span class="input-group-addon">
+                                                <i class="fa fa-calendar"></i>
+                                            </span>
+
+                                            <input type="text" name="time_end" class="form-control"
+                                                value="<?php  echo $lookzoom['tgl_end_time']; ?>" data-provide=""
+                                                readonly>
+                                            <span class="input-group-addon"><i class="ti-timer"></i></span>
+                                        </div>
+                                    </div>
+                                </div>
+
+                                <div class="row">
+                                    <div class="form-group col-md-12">
+                                        <label>Jumlah Peserta </label>
+                                        <input type="number" class="form-control" name="jumlah_peserta"
+                                            value="<?php  echo $lookzoom['jumlah_peserta']; ?>" readonly>
+                                    </div>
+                                </div>
+
+
+
+
+
+                                <hr>
+
+
+
+                            </div>
+
+                            <div class="col-md-6">
+
+
+
+                                <div class="row">
+
+                                    <?php if($lookzoom['status']==1) { ?>
+                                    <div class="form-group col-md-12">
+                                        <label>Deskripsi Link Zoom </label>
+                                        <button name="copiesdi" class="btn btn-primary btn-block">Copy
+                                            Deskripsi</button>
+                                        <HR />
+                                        <span id="okeee"> <?php echo $lookzoom['reply']; ?> </span>
+                                    </div>
+                                    <?php } else if($lookzoom['status']==0) { ?>
+
+                                    <div class="form-group col-md-12">
+                                        <label>Deskripsi Link Zoom </label>
+                                        <button name="-" class="btn btn-danger btn-block">Belum Disetujui</button>
+                                        <HR />
+                                    </div>
+
+
+                                    <?php } ?>
+                                </div>
+
+                            </div>
+
+
+                            <?php } else { ?>
+
+
+
+                            <div class="col-md-12">
+                                <div class="row">
+                                    <div class="form-group col-md-3">
+                                        <h1> Anda tidak mempunyai akses ini </h1>
+                                    </div>
+                                </div>
+
+
+
+
+                                <?php } ?>
+
+
+
+
+
+
+
+                            </div>
+                        </div>
+
+
+
                     </div>
-					<div class="row">
-                      
-					  <div class="form-group col-md-12">
-						<label>Waktu Mulai</label>
-						<div class="input-group" >
-						
-						</div>
-						 <div class="input-group"  >
-						 
-						  <input type="text" name="tgl_start" class="form-control" readonly value="<?php  echo $lookzoom['tgl_start_tgl']; ?>" >
-						  <span class="input-group-addon">
-							<i class="fa fa-calendar"></i>
-						  </span>
-	
-	
-		
-						  <input type="text" name="time_start" class="form-control" value="<?php  echo $lookzoom['tgl_start_time']; ?>"  readonly>
-						  <span class="input-group-addon"><i class="ti-timer"></i></span>
-						</div>
-					  </div>
-					 
-					  
-					  
-					  
-                    </div>
-					
-					
-					<div class="row">
-                      
-					  <div class="form-group col-md-12">
-						<label>Waktu Selesai</label>
-						<div class="input-group" >
-						
-						</div>
-						 <div class="input-group" >
-						 
-						  <input type="text" name="tgl_end" class="form-control" data-provide="" value="<?php  echo $lookzoom['tgl_end_tgl']; ?>" readonly>
-						  <span class="input-group-addon">
-							<i class="fa fa-calendar"></i>
-						  </span>
-	
-						  <input type="text" name="time_end" class="form-control" value="<?php  echo $lookzoom['tgl_end_time']; ?>" data-provide="" readonly >
-						  <span class="input-group-addon"><i class="ti-timer"></i></span>
-						</div>
-					  </div>
-                    </div>
-					
-					<div class="row">
-                      <div class="form-group col-md-12">
-                        <label>Jumlah Peserta </label>
-						<input type="number" class="form-control" name="jumlah_peserta"  value="<?php  echo $lookzoom['jumlah_peserta']; ?>" readonly >
-                      </div>
-                    </div>
-					
-					
-					
 
-					
-					<hr>
-					
-					
-					
-					</div>
-					
-					<div class="col-md-6">
-					
-
-					
-						<div class="row">
-						  
-						  <?php if($lookzoom['status']==1) { ?>
-						  <div class="form-group col-md-12">
-							<label>Deskripsi Link Zoom </label>
-							<button name="copiesdi" class="btn btn-primary btn-block">Copy Deskripsi</button>  	
-							<HR/>
-							<span id="okeee"> <?php echo $lookzoom['reply']; ?> </span>
-						  </div>
-						  <?php } else if($lookzoom['status']==0) { ?>
-						  
-						   <div class="form-group col-md-12">
-							<label>Deskripsi Link Zoom </label>
-							<button name="-" class="btn btn-danger btn-block">Belum Disetujui</button>  	
-							<HR/>
-						  </div>
-						  
-						  
-						   <?php } ?>
-						</div>
-					
-					</div>
-				  
-				  
-				  <?php } else { ?>
-				  
-				  
-				  
-				  	 <div class="col-md-12">
-					<div class="row">
-                      <div class="form-group col-md-3">
-						<h1> Anda tidak mempunyai akses ini </h1>
-                      </div>
-                    </div>
-					
-					
-				  
-				  
-				  <?php } ?>
-
-
-
-
-				  
-                  
 
                 </div>
-              </div>
+                <!--/.row -->
 
-           
 
-	</div>
-       
-			
-</div><!--/.row -->
-		 
-		 
-		 
-		 
-		 
-		 
+
+
+
+
+            </div>
         </div>
-      </div><!--/.main-content -->
+        <!--/.main-content -->
 
 
-     
 
-    </main>
-    <!-- END Main container -->
-	
-	
-	
+
+</main>
+<!-- END Main container -->
