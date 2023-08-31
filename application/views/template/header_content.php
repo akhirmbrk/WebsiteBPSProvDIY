e2fab73bb49a7edfec9ca3bfb95c62fa13858253
akhirmbrk <222011698@stis.ac.id>
2023-08-31 09:36:21 +0700

    fix header content

--- application/views/template/header_content.php
+++ application/views/template/header_content.php
@@ -8,4 +8,34 @@
                     </strong></h1>
             </div>
         </div>
+
+        <?php  
+        if(isset($dash)){ ?>
+        <div class="header-action">
+            <div class="flexbox align-items-center gap-items-4">
+                <a class="text- " href="#" data-calendar="prev"><i class="ti-angle-left"></i></a>
+                <span class="text-  fs-16" id="calendar-title" style="color:aliceblue"></span>
+                <a class="text-" href="#" data-calendar="next"><i class="ti-angle-right"></i></a>
+            </div>
+
+            <nav class="nav">
+                <a class="nav-link" href="#" data-calendar="today">Today</a>
+                <a class="nav-link active" href="#" data-calendar-view="month">Month</a>
+                <a class="nav-link" href="#" data-calendar-view="basicWeek">Week</a>
+                <a class="nav-link" href="#" data-calendar-view="basicDay">Day</a>
+                <div class="dropdown">
+                    <a class="nav-link dropdown-toggle" data-toggle="dropdown" href="#">More</a>
+                    <div class="dropdown-menu dropdown-menu-right">
+                        <a class="dropdown-item" href="#" data-calendar-view="agendaWeek">Agenda week</a>
+                        <a class="dropdown-item" href="#" data-calendar-view="agendaDay">Agenda day</a>
+                        <a class="dropdown-item" href="#" data-calendar-view="listYear">List year</a>
+                        <a class="dropdown-item" href="#" data-calendar-view="listMonth">List month</a>
+                        <a class="dropdown-item" href="#" data-calendar-view="listWeek">List week</a>
+                        <a class="dropdown-item" href="#" data-calendar-view="listDay">List day</a>
+                    </div>
+                </div>
+            </nav>
+        </div>
+        <?php }
+        ?>
 </main>
--- application/views/zoom/zoomindex.php
+++ application/views/zoom/zoomindex.php
@@ -1,38 +1,12 @@
 <!-- Main container -->
-<main>
-    <div class="header-action">
-        <div class="flexbox align-items-center gap-items-4">
-            <a class="text- " href="#" data-calendar="prev"><i class="ti-angle-left"></i></a>
-            <span class="text-  fs-16" id="calendar-title" style="color:aliceblue"></span>
-            <a class="text-" href="#" data-calendar="next"><i class="ti-angle-right"></i></a>
-        </div>
+</header>
 
-        <nav class="nav">
-            <a class="nav-link" href="#" data-calendar="today">Today</a>
-            <a class="nav-link active" href="#" data-calendar-view="month">Month</a>
-            <a class="nav-link" href="#" data-calendar-view="basicWeek">Week</a>
-            <a class="nav-link" href="#" data-calendar-view="basicDay">Day</a>
-            <div class="dropdown">
-                <a class="nav-link dropdown-toggle" data-toggle="dropdown" href="#">More</a>
-                <div class="dropdown-menu dropdown-menu-right">
-                    <a class="dropdown-item" href="#" data-calendar-view="agendaWeek">Agenda week</a>
-                    <a class="dropdown-item" href="#" data-calendar-view="agendaDay">Agenda day</a>
-                    <a class="dropdown-item" href="#" data-calendar-view="listYear">List year</a>
-                    <a class="dropdown-item" href="#" data-calendar-view="listMonth">List month</a>
-                    <a class="dropdown-item" href="#" data-calendar-view="listWeek">List week</a>
-                    <a class="dropdown-item" href="#" data-calendar-view="listDay">List day</a>
-                </div>
-            </div>
-        </nav>
+<div class="main-content">
+    <div class="card card-body">
+        <div id="calendar" data-provide="fullcalendar"></div>
     </div>
-    </header>
-
-    <div class="main-content">
-        <div class="card card-body">
-            <div id="calendar" data-provide="fullcalendar"></div>
-        </div>
-    </div>
-    <!--/.main-content -->
+</div>
+<!--/.main-content -->
 
 
 
