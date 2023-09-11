a192dfadf2a3f00b47969801d9255f56125a4f69
akhirmbrk <akhirmbrk@mail.ugm.ac.id>
2023-09-11 15:32:16 +0700

    landing dan dashboard monitoring

--- application/views/monitoring/indexView.php
+++ application/views/monitoring/indexView.php
@@ -2,10 +2,10 @@
 <div class="min-h-fullscreen bg-img " style=" background-image: url(<?= base_url('');
 																	?>/assets/img/bg/bg_bps.png); " data-overlay="7">
 
-	<div class="center-vh mt-250 mx-20">
-		<div class="card card-round card-shadowed px-50 py-30  mb-0 center-vh " style="max-width: 100%; opacity:70%; ">
-			<h2>Website Monitoring Kegiatan</h2>
-			<h5>BPS Provinsi DI Yogyakarta</h5>
+	<div class="center-vh mt-200 mx-20">
+		<div class="card card-round card-shadowed center-vh pt-40 pb-40" style="max-width: 100%; opacity:85%; border-radius: 36px; width:400px; height:200px;box-shadow: rgba(0, 0, 0, 0.5) 0px 5px 10px 0px inset">
+			<h2 style="font-family: 'var(--font-markazi-text)'">Website Monitoring Kegiatan</h2>
+			<h5 style="font-family: 'var(--font-markazi-text)'">BPS Provinsi DI Yogyakarta</h5>
 		</div>
 
 	</div>
