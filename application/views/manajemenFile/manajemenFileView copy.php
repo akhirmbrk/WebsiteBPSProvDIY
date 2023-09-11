da4403ddee9db6fcab880ea8abaecba16e6b4fe7
Angsss <bdprsnme@gmail.com>
2023-09-11 13:57:20 +0700

    modal tambah kegiatan sama edit font login view

--- application/views/manajemenFile/manajemenFileView copy.php
+++ application/views/manajemenFile/manajemenFileView copy.php
@@ -88,7 +88,71 @@
 						</div>
 					</form>
 				</div>
+
+				<div class="accordion accordion-flush" id="accordionFlushExample">
+					<div class="accordion-item">
+						<h2 class="accordion-header" id="flush-headingOne">
+							<button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#flush-collapseOne" aria-expanded="false" aria-controls="flush-collapseOne">
+								Tambah Kegiatan
+							</button>
+						</h2>
+						<div id="flush-collapseOne" class="accordion-collapse collapse" aria-labelledby="flush-headingOne" data-bs-parent="#accordionFlushExample">
+							<div class="accordion-body">
+								<!-- Isi Accordion -->
+
+								<form class="card-body">
+									<div class="form-group">
+										<label>Periode</label>
+										<select title="Periode" multiple data-provide="selectpicker" data-width="100%">
+											<option>2023</option>
+											<option>2022</option>
+											<option>2021</option>
+										</select>
+									</div>
+
+
+									<div class="form-group">
+										<label>Tim Kerja</label>
+										<select title="All Tim Kerja" multiple data-provide="selectpicker" data-width="100%">
+											<option>Analisis Statistik dan Penjaminan Kualitas</option>
+											<option>Neraca Regional</option>
+											<option>Statistik Sosial</option>
+											<option>Statistik Distribusi</option>
+											<option>Statistik Produksi</option>
+											<option>Sensus Pertanian & Statistik Perikanan, Pertanian dan Kehutanan (SP2K)</option>
+											<option>Pengolahan dan TIK</option>
+											<option>Diseminasi Statistik</option>
+											<option>Pembinaan dan Pelaksanaan Statistik Sektoral</option>
+											<option>Perencanaan dan Administrasi Keuangan</option>
+											<option>Manajemen SDM dan Hukum</option>
+											<option>Fasilitasi Operasional Perkantoran</option>
+											<option>SAKIP</option>
+											<option>Humas & Unit Kerja Pimpinan</option>
+											<option>Zona Integritas dan Reformasi Birokrasi</option>
+											<option>Kolaborasi Mengawal Perubahan</option>
+										</select>
+									</div>
+
+									<label>Nama Kegiatan</label>
+									<input type="text" class="form-control" id="input-required" fdprocessedid="wa148z">
+
+
+									<hr>
+
+									<div class="text-right">
+										<button class="btn btn-sm btn-bold btn-primary" type="submit">Submit</button>
+									</div>
+								</form>
+							</div>
+						</div>
+					</div>
+
+
+				</div>
 			</div>
+
+
+
 			<!-- END Filter -->
 
 			<div class="col-md-8 col-xl-9">
@@ -115,7 +179,7 @@
 
 								</div>
 								<div class="col">
-									<button class="btn btn-float btn-sm btn-primary" fdprocessedid="d1yj1q" data-provide="tooltip" title="" data-perform="delete" data-target="#" data-original-title="Tambah Kegiatan"><i class="fa fa-plus"></i></button>
+									<button class="btn btn-float btn-sm btn-primary" fdprocessedid="d1yj1q" data-provide="tooltip" title="" data-toggle="modal" data-target="#modal-subkegiatan" data-original-title="Tambah Kegiatan"><i class="fa fa-plus"></i><label></label></button>
 								</div>
 
 							</div>
@@ -144,7 +208,7 @@
 															<td>Mark</td>
 															<td class="text-right table-actions">
 																<nav class="nav gap-2 fs-16 justify-content-center">
-																	<a class="nav-link hover-secondary cat-delete" href="#" data-provide="tooltip" title="" data-perform="delete" data-target="#" data-original-title="View"><i class="fa fa-eye"  style="text-decoration: none;"></i></a>
+																	<a class="nav-link hover-secondary cat-delete" href="#" data-provide="tooltip" title="" data-perform="delete" data-target="#" data-original-title="View"><i class="fa fa-eye" style="text-decoration: none;"></i></a>
 																	<a class="nav-link hover-success" href="#" data-provide="tooltip" title="" data-target="#" data-original-title="Download"><i class="fa fa-download"></i></a>
 																</nav>
 															</td>
@@ -304,7 +368,7 @@
 
 								</div>
 								<div class="col">
-									<button class="btn btn-float btn-sm btn-primary" fdprocessedid="d1yj1q" data-provide="tooltip" title="" data-perform="delete" data-target="#" data-original-title="Tambah Kegiatan"><i class="fa fa-plus"></i></button>
+									<button class="btn btn-float btn-sm btn-primary" fdprocessedid="d1yj1q" data-provide="tooltip" title="" data-toggle="modal" data-target="#modal-subkegiatan" data-original-title="Tambah Kegiatan"><i class="fa fa-plus"></i><label></label></button>
 								</div>
 
 							</div>
@@ -510,6 +574,7 @@
 				</button>
 			</div>
 			<div class="modal-body">
+
 				<div class="form-group">
 					<!-- <label for="input-placeholder">Nama Dokumen</label> -->
 					<input type="text" class="form-control" placeholder="Nama Dokumen" id="input-placeholder" fdprocessedid="74vtj7">
@@ -520,9 +585,45 @@
 					<span class="input-group-append">
 						<button class="btn btn-light file-browser" type="button"><i class="fa fa-upload"></i></button>
 					</span>
-				
+
 				</div>
 				<small class="form-text"><i>Dokumen harus dalam format .pdf</i></small>
+				<br>
+				<button class="btn btn-label btn-primary" fdprocessedid="n94e2j"><label><i class="ti-check"></i></label> Submit</button>
+			</div>
+		</div>
+	</div>
+</div>
+
+<div class="modal fade modal-top" id="modal-subkegiatan" tabindex="-1" role="dialog" aria-labelledby="modal-sqduh-label" aria-hidden="true">
+	<div class="modal-dialog modal-sm">
+		<div class="modal-content">
+			<div class="modal-header">
+				<h5 class="modal-title" id="modal-sqduh-label">Tambah Sub-Kegiatan</h5>
+				<button type="button" class="close" data-dismiss="modal" aria-label="Close">
+					<span aria-hidden="true">&times;</span>
+				</button>
+			</div>
+			<div class="modal-body">
+
+				<div class="form-group">
+					<!-- <label for="input-placeholder">Nama Dokumen</label> -->
+					<input type="text" class="form-control" value="Kegiatan" disabled="" fdprocessedid="q5py2">
+				</div>
+
+				<div class="form-group">
+					<label class="require" for="input-required">Nama Sub-Kegiatan</label>
+					<input type="text" class="form-control" id="input-required" fdprocessedid="aqnmfw">
+				</div>
+				<!-- <div class="input-group file-group">
+					<input type="text" class="form-control file-value" placeholder="Choose file..." readonly>
+					<input type="file" multiple>
+					<span class="input-group-append">
+						<button class="btn btn-light file-browser" type="button"><i class="fa fa-upload"></i></button>
+					</span>
+
+				</div> -->
+
 				<br>
 				<button class="btn btn-label btn-primary" fdprocessedid="n94e2j"><label><i class="ti-check"></i></label> Submit</button>
 			</div>
