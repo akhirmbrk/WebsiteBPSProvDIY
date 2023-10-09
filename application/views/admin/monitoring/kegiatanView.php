605c56147194f5e551b8730e71bba49d0259ef67
muhammadAfnan2 <222011494@stis.ac.id>
2023-10-09 08:48:44 +0700

    push puh

--- application/views/admin/monitoring/kegiatanView.php
+++ application/views/admin/monitoring/kegiatanView.php
@@ -1,9 +1,6 @@
-<head>
-	<link rel="stylesheet" type="text/css" href="<?php echo base_url(); ?>assets/css/style.css" />
-</head>
 <!-- Main container -->
 
-<header class="header header-inverse mb-0" style="background-image: url(<?= base_url('assets/img/bg/redhead.png') ?>);">
+<header class="header header-inverse mb-0" style="background:rgba(243,243,243,255);">
 	<div class="container">
 		<div class="header-info">
 			<div class="left">
@@ -31,114 +28,79 @@ text-shadow: 1px 0px 1px #CCCCCC, 0px 1px 1px #EEEEEE, 2px 1px 1px #CCCCCC, 1px 
 	</div>
 </header>
 <!--/.header -->
-<section>
-	<div class="main-content">
-		<div class="set">
-			<div><img src="<?php echo base_url(); ?>/assets/img/leaves/snow1.png" /></div>
-			<div><img src="<?php echo base_url(); ?>/assets/img/leaves/snow2.png" /></div>
-			<div><img src="<?php echo base_url(); ?>/assets/img/leaves/snow3.png" /></div>
-			<div><img src="<?php echo base_url(); ?>/assets/img/leaves/snow4.png" /></div>
-			<div><img src="<?php echo base_url(); ?>/assets/img/leaves/snow1.png" /></div>
-			<div><img src="<?php echo base_url(); ?>/assets/img/leaves/snow2.png" /></div>
-			<div><img src="<?php echo base_url(); ?>/assets/img/leaves/snow3.png" /></div>
-			<div><img src="<?php echo base_url(); ?>/assets/img/leaves/snow4.png" /></div>
-		</div>
-		<div class="set set2">
-			<div><img src="<?php echo base_url(); ?>/assets/img/leaves/snow1.png" /></div>
-			<div><img src="<?php echo base_url(); ?>/assets/img/leaves/snow2.png" /></div>
-			<div><img src="<?php echo base_url(); ?>/assets/img/leaves/snow3.png" /></div>
-			<div><img src="<?php echo base_url(); ?>/assets/img/leaves/snow4.png" /></div>
-			<div><img src="<?php echo base_url(); ?>/assets/img/leaves/snow1.png" /></div>
-			<div><img src="<?php echo base_url(); ?>/assets/img/leaves/snow2.png" /></div>
-			<div><img src="<?php echo base_url(); ?>/assets/img/leaves/snow3.png" /></div>
-			<div><img src="<?php echo base_url(); ?>/assets/img/leaves/snow4.png" /></div>
-		</div>
-		<div class="set set3">
-			<div><img src="<?php echo base_url(); ?>/assets/img/leaves/snow1.png" /></div>
-			<div><img src="<?php echo base_url(); ?>/assets/img/leaves/snow2.png" /></div>
-			<div><img src="<?php echo base_url(); ?>/assets/img/leaves/snow3.png" /></div>
-			<div><img src="<?php echo base_url(); ?>/assets/img/leaves/snow4.png" /></div>
-			<div><img src="<?php echo base_url(); ?>/assets/img/leaves/snow1.png" /></div>
-			<div><img src="<?php echo base_url(); ?>/assets/img/leaves/snow2.png" /></div>
-			<div><img src="<?php echo base_url(); ?>/assets/img/leaves/snow3.png" /></div>
-			<div><img src="<?php echo base_url(); ?>/assets/img/leaves/snow4.png" /></div>
-		</div>
-		<div class="container">
-			<?php echo $this->session->flashdata('info_form');  ?>
-			<div class="row">
 
-				<div class="col-md-4 col-xl-3 d-none d-md-block">
+<div class="main-content">
 
-					<!-- Filter -->
-					<div class="card shadow-1">
-						<h5 class="card-title"><strong>Filter Kegiatan</strong></h5>
+	<div class="container">
+		<?php echo $this->session->flashdata('info_form');  ?>
+		<div class="row">
+			<div class="card">
 
-						<form class="card-body">
-							<div class="form-group">
-								<label>Periode Pelaksanaan</label>
-								<select id="filterPeriode" name="filterPeriode" title="Periode Pelaksanaan" multiple data-live-search="true" data-actions-box="true" class="selectpicker" data-provide="selectpicker" data-width="100%">
+				<!-- Filter -->
+				<form>
+					<br>
+					<div class="card-body">
+						<h4> <strong>Filter Kegiatan</strong></h4>
+						<div class="form-row"><!-- Searching kegiatan -->
+							<div class="form-group col-md-6">
+								<label for="filterPeriode">Periode Pelaksanaan</label>
+								<select id="filterPeriode" name="filterPeriode" title="Periode Pelaksanaan" multiple data-live-search="true" data-actions-box="true" class="selectpicker form-control" data-provide="selectpicker" data-width="100%">
 									<?php
 									if (count($periode)) {
 										foreach ($periode as $indeks => $item) {
-											$selected = ($item['aktif'] == 1) ? "selected" : ""; ?>
+											$selected = ($item['aktif'] == 1) ? "selected" : "";
+									?>
 											<option value="<?= $item['Tahun'] ?>" <?= $selected ?>><?= $item['Tahun']; ?></option>
-											?>
-									<?php }
-									} ?>
-
+									<?php
+										}
+									}
+									?>
 								</select>
 							</div>
-
-
-
-							<div class="form-group">
-								<label>Tim Kerja</label>
-								<select id="filterTimKerja" name="filterTimKerja" title="All Tim Kerja" multiple data-live-search="true" data-actions-box="true" class="selectpicker" data-provide="selectpicker" data-width="100%">
+							<div class="form-group col-md-6">
+								<label for="filterTimKerja">Tim Kerja</label>
+								<select id="filterTimKerja" name="filterTimKerja" title="All Tim Kerja" multiple data-live-search="true" data-actions-box="true" class="selectpicker form-control" data-provide="selectpicker" data-width="100%">
 									<?php
 									if (count($tim_kerja)) {
-										foreach ($tim_kerja as $indeks => $item) { ?>
+										foreach ($tim_kerja as $indeks => $item) {
+									?>
 											<option value="<?= $item['id_zteam'] ?>"><?php echo $item['nama_team']; ?></option>
-									<?php }
-									} ?>
+									<?php
+										}
+									}
+									?>
 								</select>
 							</div>
-
-							<hr>
-							<div class="text-right">
-								<a id="resetFilter" class="btn btn-sm btn-bold btn-secondary mr-1">Reset</a>
-							</div>
-
-						</form>
-					</div>
-				</div>
-				<!-- END Filter -->
-
-				<div class="col-md-8 col-xl-9">
-					<div class="media-list media-list-divided media-list-hover" data-provide="selectall">
-						<div class="media-list-body bg-white b-1">
-
-							<!-- Searching kegiatan -->
-							<div class="card-body">
-								<form class="lookup lookup-right">
-									<input type="text" id="searchKegiatan" name="searchKegiatan" autocomplete="off" placeholder="Cari Kegiatan">
-								</form>
-							</div>
-							<!-- end Searching kegiatan -->
-
-							<!-- List Kegiatan  -->
-							<div id="ajaxContent"></div>
-
+						</div>
+						<div class="form-group text-right">
+							<button id="resetFilter" class="btn btn-secondary btn-sm">Reset</button>
 						</div>
 					</div>
+				</form>
+
+				<!-- END Filter -->
+				<hr>
+				<div class="media-list media-list-divided media-list-hover" data-provide="selectall">
+					<div class="media-list-body b-1 mb-30">
+
+						<div class="lookup lookup-right m-3">
+							<input type="text" id="searchKegiatan" name="searchKegiatan" autocomplete="off" placeholder="Cari Kegiatan">
+						</div>
+
+						<!-- List Kegiatan  -->
+						<div id="ajaxContent"></div>
+
+					</div>
+				</div>
 
 
-					<!-- <footer class="flexbox align-items-center py-20">
+				<!-- <footer class="flexbox align-items-center py-20">
                 </footer> -->
 
-				</div>
+
 
 			</div>
 
 		</div>
 	</div>
-</section>
+</div>
