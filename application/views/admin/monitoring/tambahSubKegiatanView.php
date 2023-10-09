6f70d15c626278b40f751cc5c50fcfc5841aa586
muhammadAfnan2 <222011494@stis.ac.id>
2023-10-09 13:06:00 +0700

    good bgt

--- application/views/admin/monitoring/tambahSubKegiatanView.php
+++ application/views/admin/monitoring/tambahSubKegiatanView.php
@@ -102,7 +102,7 @@ text-shadow: 1px 0px 1px #CCCCCC, 0px 1px 1px #EEEEEE, 2px 1px 1px #CCCCCC, 1px 
 
 
   							<footer class="card-footer text-right">
-  								<a class="btn btn-secondary mr-2" href="<?= base_url('Monitoring/Kegiatan') ?>">Cancel</a>
+  								<a class="btn btn-secondary mr-2" href="<?= base_url('Admin/Monitoring/Kegiatan') ?>">Cancel</a>
   								<button class="btn btn-primary" type="submit">Submit</button>
   							</footer>
 
