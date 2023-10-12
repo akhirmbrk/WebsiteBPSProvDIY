1eeaea98d12f2edc3fe1f866a51aa4e5b44d187e
akhirmbrk <akhirmbrk@mail.ugm.ac.id>
2023-10-12 10:28:16 +0700

    fluid + tambah anggota tim kerja

--- application/views/admin/monitoring/detailTimKerjaView.php
+++ application/views/admin/monitoring/detailTimKerjaView.php
@@ -1,84 +1,84 @@
  <main>
  	<header class="header header-inverse mb-0" style="background:rgba(243,243,243,255)">
- 		<div class="container">
- 			<div class="header-info">
- 				<div class="left">
- 					<br>
- 					<h2 class="header-title" style="font-family: 'Acme', sans-serif;
+ 		<!-- <div class="container"> -->
+ 		<div class="header-info">
+ 			<div class="left">
+ 				<br>
+ 				<h2 class="header-title" style="font-family: 'Acme', sans-serif;
 						font-size: 55px;
 					    color: #444448;
 				        text-shadow: 1px 0px 1px #CCCCCC, 0px 1px 1px #EEEEEE, 2px 1px 1px #CCCCCC, 1px 2px 1px #EEEEEE, 3px 2px 1px #CCCCCC, 2px 3px 1px #EEEEEE, 4px 3px 1px #CCCCCC, 3px 4px 1px #EEEEEE, 5px 4px 1px #CCCCCC, 4px 5px 1px #EEEEEE, 6px 5px 1px #CCCCCC, 5px 6px 1px #EEEEEE, 7px 6px 1px #CCCCCC;">Anggota Tim Kerja <small class="subtitle"></small></h2>
- 				</div>
- 			</div>
-
-
- 			<div class="header-action">
- 				<?php if (!count($member)) { ?>
- 					<div class="buttons">
- 						<a class="btn btn-primary btn-float" href="<?= base_url('Admin/Monitoring/TimKerja/tambahAnggotaTimKerja/') . $idTim ?>" title="Tambah Anggota" data-provide="tooltip"><i class="ti-plus"></i></a>
- 					</div>
- 				<?php } ?>
-
  			</div>
  		</div>
+
+
+ 		<div class="header-action">
+
+ 			<div class="buttons">
+ 				<a class="btn btn-primary btn-float" href="<?= base_url('Admin/Monitoring/TimKerja/tambahAnggotaTimKerja/') . $idTim ?>" title="Tambah Anggota" data-provide="tooltip"><i class="ti-plus"></i></a>
+ 			</div>
+
+
+ 		</div>
+ 		<!-- </div> -->
  	</header><!--/.header -->
 
  	<div class="main-content">
 
- 		<div class="container">
- 			<div class="row">
+ 		<!-- <div class="container"> -->
+ 		<div class="row">
 
- 				<div class="col-md-8 col-xl-9">
- 					<?php
-						// var_dump($member[0]);
-						if (count($member)) {
-							foreach ($member as $indeks => $item) {  ?>
- 							<div class="card shadow-1">
- 								<header class="card-header bg-lightest">
- 									<div class="card-title flexbox">
- 										<img class="avatar" src="<?= base_url('') ?>/assets/img/avatar/1.jpg" alt="...">
- 										<div>
- 											<h5 class="mb-0"><?= $item['nama_peg'] ?>
- 												<small class="sidetitle fs-11"><?= $item['nip_lama'] ?></small>
- 											</h5>
- 											<small class=""><?php if ($item['id_user'] == $item['id_ketuatim']) {
-																	echo 'Ketua Tim';
-																} else {
-																	echo 'Anggota';
-																} ?></small>
- 										</div>
- 									</div>
- 								</header>
-
- 								<div class="card-body">
- 									<p></p>
- 								</div>
-
- 							</div>
- 						<?php }
-						} else { ?>
+ 			<div class="col-md-8 col-xl-12">
+ 				<?php
+					// var_dump($member[0]);
+					if (count($member)) {
+						foreach ($member as $indeks => $item) {  ?>
  						<div class="card shadow-1">
- 							<h4 class="m-3">
- 								Admin Belum menambahkan anggota Tim
- 							</h4>
+ 							<header class="card-header bg-lightest">
+ 								<div class="card-title flexbox">
+ 									<img class="avatar" src="<?= base_url('') ?>/assets/img/avatar/1.jpg" alt="...">
+ 									<div>
+ 										<h5 class="mb-0"><?= $item['nama_peg'] ?>
+ 											<small class="sidetitle fs-11"><?= $item['nip_lama'] ?></small>
+ 										</h5>
+ 										<small class=""><?php if ($item['id_user'] == $item['id_ketuatim']) {
+																echo 'Ketua Tim';
+															} else {
+																echo 'Anggota';
+															} ?></small>
+ 									</div>
+ 								</div>
+ 							</header>
+
+ 							<div class="card-body">
+ 								<p></p>
+ 							</div>
+
  						</div>
- 					<?php } ?>
+ 					<?php }
+					} else { ?>
+ 					<div class="card shadow-1">
+ 						<h4 class="m-3">
+ 							Admin Belum menambahkan anggota Tim
+ 						</h4>
+ 					</div>
+ 				<?php } ?>
 
 
 
- 					<br>
- 					<br>
-
-
- 				</div>
-
-
- 				<div class="col-md-4 col-xl-3">
-
-
- 				</div>
+ 				<br>
+ 				<br>
 
 
  			</div>
+
+
+ 			<div class="col-md-4 col-xl-3">
+
+
+ 			</div>
+
+
  		</div>
+ 		<!-- </div> -->
  	</div>
--- application/views/admin/monitoring/kegiatanView.php
+++ application/views/admin/monitoring/kegiatanView.php
@@ -1,105 +1,105 @@
 <!-- Main container -->
 <main>
 	<header class="header header-inverse mb-0" style="background:rgba(243,243,243,255);">
-		<div class="container">
-			<div class="header-info">
-				<div class="left">
-					<br>
-					<h2 class="header-title" style="font-family: 'Acme', sans-serif;
+		<!-- <div class="container"> -->
+		<div class="header-info">
+			<div class="left">
+				<br>
+				<h2 class="header-title" style="font-family: 'Acme', sans-serif;
 						 font-size: 55px;
 						  color: #444448;
 
 text-shadow: 1px 0px 1px #CCCCCC, 0px 1px 1px #EEEEEE, 2px 1px 1px #CCCCCC, 1px 2px 1px #EEEEEE, 3px 2px 1px #CCCCCC, 2px 3px 1px #EEEEEE, 4px 3px 1px #CCCCCC, 3px 4px 1px #EEEEEE, 5px 4px 1px #CCCCCC, 4px 5px 1px #EEEEEE, 6px 5px 1px #CCCCCC, 5px 6px 1px #EEEEEE, 7px 6px 1px #CCCCCC;"><strong>Kegiatan</strong> <small class="subtitle" style="color: black;
 
 text-shadow: 1px 0px 1px #CCCCCC, 0px 1px 1px #EEEEEE, 2px 1px 1px #CCCCCC, 1px 2px 1px #EEEEEE, 3px 2px 1px #CCCCCC, 2px 3px 1px #EEEEEE, 4px 3px 1px #CCCCCC, 3px 4px 1px #EEEEEE, 5px 4px 1px #CCCCCC, 4px 5px 1px #EEEEEE, 6px 5px 1px #CCCCCC, 5px 6px 1px #EEEEEE, 7px 6px 1px #CCCCCC;">Cek Kegiatan yang sedang
-							Berjalan</small></h2>
-				</div>
+						Berjalan</small></h2>
 			</div>
-			<?php // CEK ROLE USER
-			$roleRequie = [1];
-			if (count(array_intersect($roleRequie, $_SESSION['user_role'])) > 0) { ?>
-				<div class="header-action">
-					<div class="buttons">
-						<a class="btn btn-primary btn-float" href="<?= base_url('Admin/Monitoring/Kegiatan/tambahKegiatan') ?>" title="Tambah Kegiatan" data-provide="tooltip"><i class="ti-plus"></i></a>
-					</div>
-
-				</div>
-			<?php } ?>
 		</div>
+		<?php // CEK ROLE USER
+		$roleRequie = [1];
+		if (count(array_intersect($roleRequie, $_SESSION['user_role'])) > 0) { ?>
+			<div class="header-action">
+				<div class="buttons">
+					<a class="btn btn-primary btn-float" href="<?= base_url('Admin/Monitoring/Kegiatan/tambahKegiatan') ?>" title="Tambah Kegiatan" data-provide="tooltip"><i class="ti-plus"></i></a>
+				</div>
+
+			</div>
+		<?php } ?>
+		<!-- </div> -->
 	</header>
 	<!--/.header -->
 
 	<div class="main-content">
 
-		<div class="container">
-			<div class="row">
-				<?php echo $this->session->flashdata('info_form');  ?>
-				<div class="card">
+		<!-- <div class="container"> -->
+		<div class="row">
+			<?php echo $this->session->flashdata('info_form');  ?>
+			<div class="card">
 
-					<!-- Filter -->
-					<form>
-						<br>
-						<div class="card-body">
-							<h4> <strong>Filter Kegiatan</strong></h4>
-							<div class="form-row"><!-- Searching kegiatan -->
-								<div class="form-group col-md-6">
-									<label for="filterPeriode">Periode Pelaksanaan</label>
-									<select id="filterPeriode" name="filterPeriode" title="Periode Pelaksanaan" multiple data-live-search="true" data-actions-box="true" class="selectpicker form-control" data-provide="selectpicker" data-width="100%">
-										<?php
-										if (count($periode)) {
-											foreach ($periode as $indeks => $item) {
-												$selected = ($item['aktif'] == 1) ? "selected" : "";
-										?>
-												<option value="<?= $item['Tahun'] ?>" <?= $selected ?>><?= $item['Tahun']; ?></option>
-										<?php
-											}
+				<!-- Filter -->
+				<form>
+					<br>
+					<div class="card-body">
+						<h4> <strong>Filter Kegiatan</strong></h4>
+						<div class="form-row"><!-- Searching kegiatan -->
+							<div class="form-group col-md-6">
+								<label for="filterPeriode">Periode Pelaksanaan</label>
+								<select id="filterPeriode" name="filterPeriode" title="Periode Pelaksanaan" multiple data-live-search="true" data-actions-box="true" class="selectpicker form-control" data-provide="selectpicker" data-width="100%">
+									<?php
+									if (count($periode)) {
+										foreach ($periode as $indeks => $item) {
+											$selected = ($item['aktif'] == 1) ? "selected" : "";
+									?>
+											<option value="<?= $item['Tahun'] ?>" <?= $selected ?>><?= $item['Tahun']; ?></option>
+									<?php
 										}
-										?>
-									</select>
-								</div>
-								<div class="form-group col-md-6">
-									<label for="filterTimKerja">Tim Kerja</label>
-									<select id="filterTimKerja" name="filterTimKerja" title="All Tim Kerja" multiple data-live-search="true" data-actions-box="true" class="selectpicker form-control" data-provide="selectpicker" data-width="100%">
-										<?php
-										if (count($tim_kerja)) {
-											foreach ($tim_kerja as $indeks => $item) {
-										?>
-												<option value="<?= $item['id_zteam'] ?>"><?php echo $item['nama_team']; ?></option>
-										<?php
-											}
-										}
-										?>
-									</select>
-								</div>
+									}
+									?>
+								</select>
 							</div>
-							<div class="form-group text-right">
-								<button id="resetFilter" class="btn btn-secondary btn-sm">Reset</button>
+							<div class="form-group col-md-6">
+								<label for="filterTimKerja">Tim Kerja</label>
+								<select id="filterTimKerja" name="filterTimKerja" title="All Tim Kerja" multiple data-live-search="true" data-actions-box="true" class="selectpicker form-control" data-provide="selectpicker" data-width="100%">
+									<?php
+									if (count($tim_kerja)) {
+										foreach ($tim_kerja as $indeks => $item) {
+									?>
+											<option value="<?= $item['id_zteam'] ?>"><?php echo $item['nama_team']; ?></option>
+									<?php
+										}
+									}
+									?>
+								</select>
 							</div>
 						</div>
-					</form>
-
-					<!-- END Filter -->
-					<div class="media-list media-list-divided media-list-hover" data-provide="selectall">
-						<div class="lookup lookup-right m-3">
-							<input type="text" id="searchKegiatan" name="searchKegiatan" autocomplete="off" placeholder="Cari Kegiatan">
-						</div>
-
-						<!-- List Kegiatan  -->
-						<div id="ajaxContent" class="media-list-body b-1 mb-30">
-
-
-
+						<div class="form-group text-right">
+							<button id="resetFilter" class="btn btn-secondary btn-sm">Reset</button>
 						</div>
 					</div>
+				</form>
+
+				<!-- END Filter -->
+				<div class="media-list media-list-divided media-list-hover" data-provide="selectall">
+					<div class="lookup lookup-right m-3">
+						<input type="text" id="searchKegiatan" name="searchKegiatan" autocomplete="off" placeholder="Cari Kegiatan">
+					</div>
+
+					<!-- List Kegiatan  -->
+					<div id="ajaxContent" class="media-list-body b-1 mb-30">
 
 
-					<!-- <footer class="flexbox align-items-center py-20">
+
+					</div>
+				</div>
+
+
+				<!-- <footer class="flexbox align-items-center py-20">
                 </footer> -->
 
 
 
-				</div>
-
 			</div>
+
 		</div>
+		<!-- </div> -->
 	</div>
